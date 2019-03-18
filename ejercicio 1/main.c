e91612ae480ca206ddb2d3d8e4d1820bb4004bf5
alvareztomas1 <47418131+alvareztomas1@users.noreply.github.com>
2019-03-18 12:19:37 -0300

    Add files via upload

---
+++ ejercicio 1/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero;
+
+
+    printf ("ingrese un numero: ");
+    scanf("%d", &numero);
+
+    printf ("ingreso el numero %d", numero);
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+/*
+    int numero2 = 34;
+    float altura = 23.5;
+    char letra = 'a';
+
+    numero2 = altura;
+
+
+    printf ("la variable altura vale %.2f y la letra vale %c", altura , letra);
+    printf ("la variable numero vale %d y la variable numero2 vale %d\n", numero , numero2);
+    printf ("la letra vale %c\n", letra);
+
+    */
